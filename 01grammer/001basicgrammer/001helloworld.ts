f4f40d8ebfb630522332e7043e5a1e19cc324f99
廖志标 <991435344@qq.com>
2020-11-05 09:06:17 +0800

    add:增加php和typescript的基础语法

---
+++ 01grammer/001basicgrammer/001helloworld.ts
@@ -0,0 +1,12 @@
+//1.安装npm
+//2.npm install -g typescript NPM 安装 TypeScript
+
+
+var message:string = "Hello World" 
+console.log(message)
+
+//编译运行命令
+//tsc tsc 001helloworld.ts 
+//node 001helloworld.js
+
+
