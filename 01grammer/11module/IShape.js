b196b862fedf1fc081b56872aa193596507b7b05
廖志标 <991435344@qq.com>
2020-11-08 09:43:39 +0800

    add:增加ts模块定义

---
+++ 01grammer/11module/IShape.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+//导出
+// 文件名 : SomeInterface.ts
+export interface SomeInterface {
+   // 代码部分
+}
+
+
+//导入
+import someInterfaceRef = require("./SomeInterface");
+*/
+exports.__esModule = true;
---
+++ 01grammer/11module/IShape.ts
@@ -0,0 +1,17 @@
+/*
+//导出
+// 文件名 : SomeInterface.ts 
+export interface SomeInterface { 
+   // 代码部分
+}
+
+
+//导入
+import someInterfaceRef = require("./SomeInterface");
+*/
+
+
+// <reference path = "IShape.ts" /> 
+export interface IShape { 
+   draw(); 
+}
